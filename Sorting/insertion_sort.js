973e53a0a1774c69fda2df03b3281c146b8b6704
Ian Macharia <ianmachariak17@gmail.com>
2024-03-15 10:36:36 +0300

    feat: Refactored insertion sort

--- Sorting/insertion_sort.js
+++ Sorting/insertion_sort.js
@@ -1,16 +1,26 @@
-function insertionSort(arr) {
-  let i, j;
+/**
+ * Insertion sort builds the final sorted list one item at a time by repeatedly removing elements from
+ * the input data and inserting them into their correct position in the sorted part of the list.
+ *
+ * Time Complexity: O(n^2)
+ *
+ *  Insertion sort are typically used for small datasets or as components in more complex sorting algorithms.
+ * */
 
-  for (i = 1; i < arr.length; i++) {
-    let currentVal = arr[i];
-    
-    for (j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    const currentVal = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > currentVal) {
       arr[j + 1] = arr[j];
+      j--;
     }
+
     arr[j + 1] = currentVal;
-    console.log(arr);
   }
+
   return arr;
 }
 
-insertionSort([2, 1, 9, 76, 4]);
+console.log("Sorted Array: ", insertionSort([2, 1, 9, 76, 4]));
