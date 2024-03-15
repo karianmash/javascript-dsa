3b0813cf796aad0eb43d3d08a91c684d2f5ce525
Ian Macharia <ianmachariak17@gmail.com>
2024-03-15 09:50:47 +0300

    feat: Refactored bubble_sort

--- Sorting/bubble_sort.js
+++ Sorting/bubble_sort.js
@@ -1,49 +1,23 @@
-// ES6 approach!
-function bubbleSort(arr) {
-  let noSwaps, i, j;
-  let start = new Date().getTime();
-  const swap = (arr, index1, index2) => {
-    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
-  };
+/**
+ * Bubble Sort is a simple sorting algorithm that repeatedly steps through the list, compares adjacent
+ * elements, and swaps them if they are in the wrong order. It continues until no swaps are needed,
+ * indicating that the list is sorted.
+ *
+ * Despite its simplicity, Bubble Sort is inefficient on large lists and generally performs poorly compared
+ * to more advanced algorithms.
+ */
 
-  for (let i = arr.length; i > 0; i--) {
-    noSwaps = true;
-    for (let j = 0; j < i - 1; j++) {
-      console.log(arr, arr[j], arr[j + 1]);
+function bubbleSort(arr) {
+  const len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
-        swap(arr, j, j + 1);
-        noSwaps = false;
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
-    // Break from the loop if no more iterations
-    if (noSwaps) break;
-    console.log("One iteration complete!");
   }
-  let stop = new Date().getTime();
-  let elapsed = stop - start;
-  console.log("The elapsed time was: " + elapsed + " milliseconds.");
 
   return arr;
 }
 
-bubbleSort([37, 45, 29, 8, 12, 88, -3]);
-
-// Optimized BubbleSort with noSwaps
-function bubbleSort(arr) {
-  var noSwaps;
-  for (var i = arr.length; i > 0; i--) {
-    noSwaps = true;
-    for (var j = 0; j < i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        var temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-        noSwaps = false;
-      }
-    }
-    if (noSwaps) break;
-  }
-  return arr;
-}
-
-bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]);
+console.log("Sorted Array: ", bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
